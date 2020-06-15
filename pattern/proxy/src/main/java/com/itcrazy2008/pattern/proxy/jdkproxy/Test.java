710e423e74394f8f7ad846ca8b33fb81eeb90274
itcrazy2008 <itcrazy2008@163.com>
2020-06-15 12:08:47 +0800

    修改pom文件group

--- pattern/proxy/src/main/java/com/itcrazy2008/pattern/proxy/jdkproxy/Test.java
+++ pattern/proxy/src/main/java/com/itcrazy2008/pattern/proxy/jdkproxy/Test.java
@@ -1,15 +1,15 @@
-package com.itcrazy2008.pattern.proxy.jdkproxy;
-
-//JDK动态代理 一定要有接口 利用反射来现的，所以性能会有损耗
-public class Test {
-  public static void main(String[] args) {
-    JdkProxy jdkProxy = new JdkProxy();
-    IPersion proxyInstance = (IPersion)jdkProxy.createPorxyInstance(new Zhangsan());
-    String result = proxyInstance.findLove();
-    System.out.println(result);
-
-    IPersion jiuban = (IPersion)jdkProxy.createPorxyInstance(new Liuban());
-    String result1 = jiuban.findLove();
-    System.out.println(result1);
-  }
-}
+//package com.itcrazy2008.pattern.proxy.jdkproxy;
+//
+////JDK动态代理 一定要有接口 利用反射来现的，所以性能会有损耗
+//public class Test {
+//  public static void main(String[] args) {
+//    JdkProxy jdkProxy = new JdkProxy();
+//    IPersion proxyInstance = (IPersion)jdkProxy.createPorxyInstance(new Zhangsan());
+//    String result = proxyInstance.findLove();
+//    System.out.println(result);
+//
+//    IPersion jiuban = (IPersion)jdkProxy.createPorxyInstance(new Liuban());
+//    String result1 = jiuban.findLove();
+//    System.out.println(result1);
+//  }
+//}
